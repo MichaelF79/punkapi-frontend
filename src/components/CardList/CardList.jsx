4310caaa94006b4b8af6f4b465056f46eec625fc
Michael Ferreira <ferreira.mjm@gmail.com>
2021-04-27 10:32:21 +0100

    Initial setup completed

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./CardList.module.scss";
+
+const CardList = () => {
+  return (
+    <>
+      <p>CardList works</p>
+    </>
+  );
+};
+
+export default CardList;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Sidebar.module.scss";
+
+const Sidebar = () => {
+  return (
+    <>
+      <p>Sidebar works</p>
+    </>
+  );
+};
+
+export default Sidebar;
